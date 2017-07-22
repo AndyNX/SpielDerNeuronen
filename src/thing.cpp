67966049766ce98d37d3a3259c0b45276f2d5862
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2017-07-22 03:33:37 +0200

    Changes the interface of things to allow for more complex actions

--- src/thing.cpp
+++ src/thing.cpp
@@ -7,18 +7,21 @@ class thing
 {
 protected:
 	std::vector<double> position;
+	std::vector<double> velocity;	// has to be reduced in every timestep by friction
+	//std::vector<double> acceleration;
 	//std::vector<double> orientation;
-	double energy;
-	double maxEnergy; // maybe things should split into children beyond a certain energy
-	double weight;
+	double health; // should be reduced in every timestep by a small amount
+	double maxHealth; // maybe things should split into children beyond a certain energy
+	double mass;
+	
 	double maxSpeed;
 	// double strength; // could also be a function s(health), with different objects having different maxHealth
 	std::list<thing> baggage;
-	bool isActive;
+	bool isFree;
 
 	// maybe all get Functions with some random uncertainty
 	double getPosition();
-	double getWeight();
+	double getMass();
 	double getEnergy();
 
 	virtual void progress();
@@ -26,17 +29,17 @@ protected:
 	virtual void move();
 
 	virtual void look();
-	list<thing> sense(const worldMap& map)
+	std::list<thing> sense(const worldMap& map)
 	{
 		look();
 	}
 
-	virtual void plan();
-	virtual void act();
+	virtual std::vector<double> plan(const std::list<thing>& environment);
+	virtual void act(std::vector<double> plan);
 	// amount of drained energy should maybe be scale down by amount *= (own.energy - target.energy)/target.energy
 	// , but not more than factor 1.0
-	virtual void drainEnergy(thing& target);	// = eat()
-	virtual void transferEnergy(thing& target);	// = throw()
+	virtual void transferHealth(thing& target, double amount);	// = eat(), heal()
+	virtual void transferEnergy(thing& target, double amount);	// = throw()
 	virtual void pickUp(thing& target);
 	virtual void drop(thing& target);
 }
